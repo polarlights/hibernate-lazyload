e3055bb0d03201fe953de42969bc1e982f6d19a1
polarlights <godhuyang@hotmail.com>
2018-11-29 17:12:07 +0800

    init commit

---
+++ src/main/java/org/hibernate/lazyload/HibernateLazyLoadApplication.java
@@ -0,0 +1,12 @@
+package org.hibernate.lazyload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateLazyLoadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateLazyLoadApplication.class, args);
+    }
+}
