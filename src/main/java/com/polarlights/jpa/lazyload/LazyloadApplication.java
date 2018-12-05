85a1a0714593621785720fb066af0ee604b5c5fe
david.wei <david.wei@alo7.com>
2018-12-05 09:17:49 +0800

    init commit

---
+++ src/main/java/com/polarlights/jpa/lazyload/LazyloadApplication.java
@@ -0,0 +1,12 @@
+package com.polarlights.jpa.lazyload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazyloadApplication.class, args);
+	}
+}
